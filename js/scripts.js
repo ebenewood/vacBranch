41e369f91f762b827e4e1920fa4192a592f6ee45
Eben Wood <ebenewood@yahoo.com>
2016-03-16 17:27:07 -0700

    includes some branching with fades, and delays

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,26 @@
 $(function () {
-	$('form#one').submit(function (event) {
+
+	$('body').fadeIn(500);
+
+	$('form#questions').submit(function (event) {
 		event.preventDefault();
-		if ($('input#one').val() == 'yes') {
-			$('input#two').show();
+
+		var yesCount = 0;
+		var noCount = 0;
+
+		if ($('input#one').val().toLowerCase() == 'yes') {
+			yesCount += 1;
+			$('input#two').slideDown().fadeIn();
+
+		} else if
+			($('input#one').val().toLowerCase() == 'no') {
+				noCount += 1;
+				$('input#two').val('Wish you weren\'t you?')
+				$('input#two').slideDown().fadeIn();
+			} else {
+			$('h1').text('yes | no');
+			$('input#one').text('');
+
 		};
 	});
 });
